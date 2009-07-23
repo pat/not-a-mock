5a4c7e6a5660cbacbe8677494225bd570ff6638c
Rob <rob.anderton@thewebfellas.com>
2009-07-23 17:14:40 +0100

    Fix private/protected stubbing tests to actually use private/protected methods and fix stubber to stub them correctly

--- lib/not_a_mock/stubber.rb
+++ lib/not_a_mock/stubber.rb
@@ -6,13 +6,13 @@ module NotAMock
   # installed in any object.
   class Stubber
     include Singleton
-  
+
     def initialize
       @stubbed_methods = []
     end
-  
+
     # Stub +method+ on +object+ to evalutate +block+ and return the result.
-    # 
+    #
     # You should call Object#stub_method rathing than calling this directly.
     def stub_method(object, method, &block) #:nodoc:
       unless @stubbed_methods.include?([object, method])
@@ -20,7 +20,7 @@ module NotAMock
         add_hook(object, method, &block)
       end
     end
-        
+
     # Remove the stubbed +method+ on +object+.
     #
     # You should call Object#unstub_methods rather than calling this directly.
@@ -29,29 +29,27 @@ module NotAMock
         remove_hook(object, method)
       end
     end
-  
+
     # Removes all stub methods.
     def reset
       @stubbed_methods.each do |object, method|
-        remove_hook(object, method) 
+        remove_hook(object, method)
       end
       @stubbed_methods = []
     end
-    
+
   private
 
     def add_hook(object, method, &block)
-      method_exists = method_at_any_level?(object, method.to_s)
       object.meta_eval do
-        alias_method("__unstubbed_#{method}", method) if method_exists
+        alias_method("__unstubbed_#{method}", method) if object.respond_to?(method, true)
         define_method(method, &block)
       end
     end
-  
+
     def remove_hook(object, method)
-      method_exists = method_at_any_level?(object, "__unstubbed_#{method}")
       object.meta_eval do
-        if method_exists
+        if object.respond_to?("__unstubbed_#{method}", true)
           alias_method(method, "__unstubbed_#{method}")
           remove_method("__unstubbed_#{method}")
         else
@@ -59,11 +57,6 @@ module NotAMock
         end
       end
     end
-    
-    def method_at_any_level?(object, method)
-      object.respond_to?(method.to_sym)                 ||
-      object.protected_methods.include?(method.to_sym)  ||
-      object.private_methods.include?(method.to_sym)
-    end
+
   end
 end
--- spec/stub_method_spec.rb
+++ spec/stub_method_spec.rb
@@ -1,42 +1,42 @@
 require 'spec/spec_helper'
 
 describe "A stubbed method replacing an existing instance method" do
-  
+
   before do
     @object = "Hello, world!"
     @object.stub_method(:length => 42)
   end
-  
+
   it "should return the stubbed result" do
     @object.length.should == 42
   end
-  
+
   it "should return the original result after stubbing is removed" do
     @object.unstub_method(:length)
     @object.length.should == 13
   end
-  
+
   it "should return the original result after a reset" do
     NotAMock::Stubber.instance.reset
     @object.length.should == 13
   end
-  
+
   it "should return the new result if re-stubbed" do
     @object.stub_method(:length => 24)
     @object.length.should == 24
   end
-  
+
   it "should record a call to the stubbed method" do
     @object.length
     NotAMock::CallRecorder.instance.calls.should include(:object => @object, :method => :length, :args => [], :result => 42)
   end
-  
+
   it "should return a call to the stubbed method if re-stubbed" do
     @object.stub_method(:length => 24)
     @object.length
     NotAMock::CallRecorder.instance.calls.should include(:object => @object, :method => :length, :args => [], :result => 24)
   end
-  
+
   after do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
@@ -45,53 +45,53 @@ describe "A stubbed method replacing an existing instance method" do
 end
 
 describe "A stubbed method with no existing instance method" do
-  
+
   before do
     @object = "Hello, world!"
     @object.stub_method(:blah => 42)
   end
-  
+
   it "should return the stubbed result" do
     @object.blah.should == 42
   end
-  
+
   it "should raise a NoMethodError when the method is called after stubbing is removed" do
     @object.unstub_method(:blah)
     lambda { @object.blah }.should raise_error(NoMethodError)
   end
-  
+
   it "should raise a NoMethodError when the method is called after all stubbing is removed" do
     NotAMock::Stubber.instance.reset
     lambda { @object.blah }.should raise_error(NoMethodError)
   end
-  
+
   it "should record a call to the stubbed method" do
     @object.blah
     NotAMock::CallRecorder.instance.calls.should include(:object => @object, :method => :blah, :args => [], :result => 42)
   end
-  
+
   after do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
   end
-  
+
 end
 
 describe "A stubbed class method" do
-  
+
   before do
     Time.stub_method(:now => 42)
   end
-  
+
   it "should return the stubbed result" do
     Time.now.should == 42
   end
-  
+
   it "should return the original result after stubbing is removed" do
     Time.unstub_method(:now)
     Time.now.should_not == 42
   end
-  
+
   it "should return the original result after a reset" do
     NotAMock::Stubber.instance.reset
     Time.now.should_not == 42
@@ -101,31 +101,31 @@ describe "A stubbed class method" do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
   end
-  
+
 end
 
 describe "A stubbed private method" do
   before do
     class Privateer
-      private
-      
-      def self.stubbed
-        false
+      class << self
+        private
+          def stubbed
+            false
+          end
       end
     end
-    
     Privateer.stub_method(:stubbed => true)
   end
-  
+
   it "should return the stubbed result" do
     Privateer.send(:stubbed).should be_true
   end
-  
+
   it "should return the original result after stubbing is removed" do
     Privateer.unstub_method(:stubbed)
     Privateer.send(:stubbed).should be_false
   end
-  
+
   it "should return the original result after a reset" do
     NotAMock::Stubber.instance.reset
     Privateer.send(:stubbed).should be_false
@@ -140,25 +140,26 @@ end
 describe "A stubbed protected method" do
   before :each do
     class Protector
-      protected
-      
-      def self.stubbed
-        false
+      class << self
+        protected
+          def stubbed
+            false
+          end
       end
     end
-    
+
     Protector.stub_method(:stubbed => true)
   end
-  
+
   it "should return the stubbed result" do
     Protector.send(:stubbed).should be_true
   end
-  
+
   it "should return the original result after stubbing is removed" do
     Protector.unstub_method(:stubbed)
     Protector.send(:stubbed).should be_false
   end
-  
+
   it "should return the original result after a reset" do
     NotAMock::Stubber.instance.reset
     Protector.send(:stubbed).should be_false
@@ -171,63 +172,63 @@ describe "A stubbed protected method" do
 end
 
 describe "A method stubbed with a block" do
-  
+
   before do
     @object = "Hello, world!"
     @object.stub_method(:length) do |*args|
       args.reverse
     end
   end
-  
+
   it "should return the result of the block" do
     @object.length(1, 2, 3).should == [3, 2, 1]
   end
-  
+
   after do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
   end
-  
+
 end
 
 describe "A method stubbed to raise an exception" do
-  
+
   before do
     @object = "Hello, world!"
     @object.stub_method_to_raise(:length => ArgumentError)
   end
 
   it "should raise the exception when called" do
-    lambda { @object.length }.should raise_error(ArgumentError)  
+    lambda { @object.length }.should raise_error(ArgumentError)
   end
-  
+
   it "should return the original result after stubbing is removed" do
     @object.unstub_method(:length)
     @object.length.should == 13
   end
-  
+
   after do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
   end
-  
+
 end
 
 describe "Object#stub_method" do
-  
+
   it "should stub a method with a name ending in '?'" do
     @object = "Hello, world!"
     @object.stub_method(:is_great? => true)
     @object.is_great?.should be_true
   end
-  
+
   it "should stub the []= method" do
     @object = Array.new
     @object.stub_method(:[]= => nil)
     @object[0] = 7
     @object.length.should == 0
   end
-  
+
   it "should raise an ArgumentError if called with something other than a symbol or hash" do
     @object = "Hello, world!"
     lambda { @object.stub_method(7) }.should raise_error(ArgumentError)
@@ -237,5 +238,5 @@ describe "Object#stub_method" do
     NotAMock::CallRecorder.instance.reset
     NotAMock::Stubber.instance.reset
   end
-  
+
 end
